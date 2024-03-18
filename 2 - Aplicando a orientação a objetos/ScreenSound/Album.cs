4a5bf3f8f7ac9d2346f4be23435cca07afef2863
Luisa <104804096+luisacoutinho06@users.noreply.github.com>
2024-03-18 14:47:27 -0300

    Inserindo o curso 1 e o curso 2
    Inserindo o curso 1 - CRIANDO SUA PRIMEIRA APLICAÇÃO e o curso 2 -  APLICANDO ORIENTAÇÃO A OBJETO

---
+++ 2 - Aplicando a orientação a objetos/ScreenSound/Album.cs
@@ -0,0 +1,25 @@
+﻿class Album
+{
+    // Essa variável so estará disponivel dentro das chaves, já que é um campo privado. Podemos criar relacionamentos entre classes no c#.
+    // Onde a classe possui uma instância de outra classe como um de seus membros. Isso permite que a classe tenha acesso aos membros e comportamentos da classe relacionada.
+    private List<Musica> musicas = new List<Musica>();
+    public Album(string nome)
+    {
+        Nome = nome;
+    }
+    public string Nome { get; }
+    public int DuracaoTotal => musicas.Sum(m => m.Duracao);
+    public void AdicionarMusica(Musica musica)
+    {
+        musicas.Add(musica);
+    }
+    public void ExibirMusicasDoAlbum()
+    {
+        Console.WriteLine($"Lista de músicas do ábum {Nome}.");
+        foreach (var musica in musicas)
+        {
+            Console.WriteLine($"Música: {musica.Nome}.");
+        }
+        Console.WriteLine($"Para ouvir este ábum inteiro você precisa de {DuracaoTotal} segundos.");
+    }
+}
